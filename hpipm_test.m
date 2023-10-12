d61e1d80c7e12ffc0d77fcc0413e0c603c16868f
antoineleeman <antoineleeman@gmail.com>
2023-10-12 11:21:58 +0200

    new files

--- hpipm_test.m
+++ hpipm_test.m
@@ -60,7 +60,7 @@ nx = [2,3,2,1];
 nu = [1, 2,1];
 ng = [2, 1, 1, 1];
 N = 3;
-
+bo = MX.sym('bo',1,1);
 
 options = struct;
 options.hpipm.iter_max = 100;
@@ -69,12 +69,12 @@ options.hpipm.res_b_max = 1e-10;
 options.hpipm.res_d_max = 1e-10;
 options.hpipm.res_m_max = 1e-10;
 
-solver = conic('solver', 'hpipm', struct('a',A.sparsity(), 'h', H.sparsity()),options);
+solver = qpsol('solver', 'hpipm', struct('a',A.sparsity(), 'h', H.sparsity()),options);
 
 g = [1;1;0.2;0.4;1;0.5;0.3;1;0.6;1;1;0.7];
 lbg =[0;0;0;-2;-2;0;0;-2;0;-2;-2];
 ubg = [0;0;0;2;2;0;0;2;0;2;2];
 lbx = [0.5;0.2;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1];
-ubx = [0.5;0.2; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1];
+ubx = [0.5;0.2; 1; 1; 1; 1; 1; 1; 1; 1; 1; bo];
 
-sol = solver('a',A,'h',H,'lba',lbg,'uba',ubg,'g',g,'lbx',lbx,'ubx',ubx)
+sol = solver('a',A,'h',H,'lba',lbg,'uba',ubg,'g',g,'lbx',lbx,'ubx',ubx);
