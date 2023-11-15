b064fd25a0dd9ee88cee0b9724573e13a27212d9
Antoine Leeman <antoineleeman@gmail.com>
2023-11-15 16:54:42 +0100

    bug in the forward solve

--- KKT_SLS.m
+++ KKT_SLS.m
@@ -30,7 +30,6 @@ classdef KKT_SLS < OCP
         A_dyn_current;
         B_dyn_current;
         C_cons_current;
-        D_cons_current;
     end
     
     methods
@@ -39,7 +38,9 @@ classdef KKT_SLS < OCP
             obj.current_bo = zeros(m.ni,N+1); % there should not be a bo for the last input!
             obj.current_adj_corr = 0;
             obj.epsilon = 1e-3;
-            obj = obj.initialize_solver_forward('qpoases');% try osqp!
+           %obj = obj.initialize_solver_forward('qpoases');% try osqp! + HPIPM
+            obj = obj.initialize_solver_forward('osqp');
+
             obj.Q_reg = 1e-3*eye(m.nx);
             obj.R_reg = 1e-3*eye(m.nu);
             
@@ -172,14 +173,14 @@ classdef KKT_SLS < OCP
                 
                 % Cxuk missing!
                 A = obj.A_dyn_current{kk};
-                B = obj.A_dyn_current{kk};
+                B = obj.B_dyn_current{kk};
                 K{kk} = -(Cuk+B'*S{kk+1}*B)\(B'*S{kk+1}*A);
                 S{kk} = Cxk + A'*S{kk+1}*A + A'*S{kk+1}*B*K{kk};
             end
             obj.K_current = K;            
         end
         % 
-        function [obj, beta] = compute_backoff(obj)
+        function [obj, beta_kj] = udpate_backoff(obj)
             m=obj.m;
             N = obj.N;
             nx = m.nx;
@@ -190,17 +191,27 @@ classdef KKT_SLS < OCP
 
             Phi_x_kj{1} = m.E;% should be the disturbance matrix
 
-            beta_kj = zeros(m.ni,obj.N);
+            beta_kj = zeros(m.ni,N);
             C = m.C;
             for kk=1:N
                 DIM=1; %double check
                 Phi_u_kj{kk} = obj.K_current{kk}*Phi_x_kj{kk}; %% should have only one input, Riccati recursion is probably wrong
 
-                beta_kj(:,kk) = vecnorm(C'*[Phi_x_kj{kk};Phi_u_kj{kk}],2,DIM);
+                beta_kj(:,kk) = vecnorm(C*full([Phi_x_kj{kk};Phi_u_kj{kk}]),2,2);
                 bo_k(:,kk) = beta_kj(:,kk); % needs an extra loop over j
                 Phi_x_kj{kk+1} = (obj.A_dyn_current{kk} + obj.B_dyn_current{kk}*obj.K_current{kk})*Phi_x_kj{kk};
                 % calculate beta here + bo here
             end
+            Cf = m.Cf;
+
+            % we don't do the tightening on the last stage
+            %beta_kj(1:4,N+1) = [vecnorm(Cf*full(Phi_x_kj{N+1}),2,2)]; % HARDCODED VALUE!!! We should have a separate variable beta for the last state. It could have more constraints.
+            obj.beta_kj = beta_kj;
+
+
+            update_ubg = reshape([zeros(nx,N); [m.d - beta_kj]],[(N)*(ni+nx),1]);
+%            new_ubg = obj.nominal_ubg - update_ubg;
+            obj.ubg_current = update_ubg;
         end
 
         % function S_cons = blockConstraint(obj,x,u)
@@ -210,7 +221,7 @@ classdef KKT_SLS < OCP
         %         m.C(x,u), m.D(x,u), zeros(m.ni,m.nx)];
         % end
 
-        function obj = solve_beta_stationnarity(obj)
+        function obj = update_cost_tube(obj)
             for kk=1:obj.N
                 obj.eta_kj(:,kk) = obj.mu_current(:,kk)./sqrt(obj.beta_kj(:,kk));
             end
