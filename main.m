7cc261839ad5e9d96e4704bf26ed65c3ab8974f7
Antoine Leeman <antoineleeman@gmail.com>
2023-11-20 08:33:25 +0100

    new solve function

--- main.m
+++ main.m
@@ -5,13 +5,14 @@ clc;
 addpath(genpath('../casadi-3.6.3-osx64-matlab2018b'));
 import casadi.*
 m = Integrator();
-x0 = [-5;5];
-N =10;
+x0 = [-2;5];
+N =50;
 Q = eye(m.nx);
 R = 100*eye(m.nu);
 Qf = Q;
 
 kkt = KKT_SLS(N,Q,R,m, x0,Qf); %x0 seems unused
+kkt.solve();
 
 figure(1);
 clf;
@@ -21,33 +22,4 @@ axis equal;
 
 figure(2);
 clf;
-MAX_ITER = 5;
 
-for ii=1:MAX_ITER
-
-    
-    
-    [kkt, x_bar, u_bar, lambda_bar, mu_bar] = kkt.forward_solve(x0);
-    
-    % lambda_bar: dynamics
-    % mu_bar: constraints % not sure about their value, are the constraints on
-    % x really not active?
-    x_bar_plot = full(x_bar);
-    u_bar_plot = full(u_bar);
-
-    figure(1);
-    plot(x_bar_plot(1,:)+ii,x_bar_plot(2,:),'.-');
-    hold on;
-
-    figure(2);
-    hold on;
-     plot(u_bar_plot);
-    
-    
-    kkt = kkt.update_cost_tube();
-    
-    [kkt, K] = kkt.backward_solve();
-
-    [kkt,beta] = kkt.udpate_backoff();
-
-end
