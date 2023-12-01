9ef4c3c76f2051020e3de9a6e7136ef03142a05c
Antoine Leeman <antoineleeman@gmail.com>
2023-12-01 16:27:21 +0100

    with constraints, and seems everything converge

--- expe03.m
+++ expe03.m
@@ -4,13 +4,13 @@ clear all;
 close all;
 clc;
 
-L = 12;
+L = 6;
 msd = ChainOfMassSpringDampers(L);
-Q = 100*eye(msd.nx);
+Q = 10*eye(msd.nx);
 R = eye(msd.nu);
 Qf = Q;
 n_sample = 10000;
-N=30;
+N=20;
 
 
 
@@ -18,8 +18,12 @@ kkt = KKT_SLS(N,Q,R,msd,Qf);
 it_kkt = [];
 
 for ii =1:n_sample
+
     ii
     x0 =rand(msd.nx,1);
+    %x0 = -0.99*3*[ones(msd.nx,1)];
+    %x0 = kron(ones(msd.nx/2,1),[1;0]);
+    %x0= [0;1;zeros(msd.nx-2,1)];
     tic
     [feasible,it] = kkt.solve(x0);
     time =toc;
@@ -28,12 +32,13 @@ for ii =1:n_sample
     end
 end
 
-save(getUniqueName('it_kkt'),'it_kkt','msd');
-%%
-clear all
-close all
-clf
-load('26-Nov-2023_17_23_21__it_kkt.mat')
+%save(getUniqueName('it_kkt'),'it_kkt','msd');
+%
+%clear all
+%close all
+%clf
+%load('26-Nov-2023_17_23_21__it_kkt.mat')
+%load('28-Nov-2023_13_23_07__it_kkt.mat')
 colors = [0.0504    0.0298    0.5280
     0.4934    0.0115    0.6580
     0.7964    0.2780    0.4713
