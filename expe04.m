29cb01cf0c248fe00ed0d3275466c6a0ecb59d36
Antoine Leeman <antoineleeman@gmail.com>
2024-03-06 14:11:41 -0500

    new experiment to verify it yields the same solution

---
+++ expe04.m
@@ -0,0 +1,44 @@
+% File: expe04.m
+% Author: Antoine Leeman (aleeman@ethz.ch)
+% Date: 06th March 2024
+% License: MIT
+% Reference:
+%{
+@article{leeman2024fast,
+  title={Fast System Level Synthesis: Robust Model Predictive Control using Riccati Recursions},
+  author={Leeman, Antoine P and K{\"o}hler, Johannes and Messerer, Florian and Lahr, Amon and Diehl, Moritz and Zeilinger, Melanie N},
+  journal={arXiv preprint arXiv:2401.13762},
+  year={2024}}
+%}
+% Link: https://arxiv.org/abs/2401.13762
+% -----------------------------------------------------------------------------
+%%
+clear all;
+close all;
+clc;
+
+L = 25;
+msd = ChainOfMassSpringDampers_actuated(L);
+Q = 3*eye(msd.nx);
+R = eye(msd.nu);
+Qf = Q;
+n_sample = 1000;
+N=25;
+
+kkt = KKT_SLS(N,Q,R,msd,Qf);
+it_kkt = [];
+
+for ii =1:n_sample
+    ii
+    x0 =4*rand(msd.nx,1)-2;
+    tic
+    [feasible,it] = kkt.solve(x0);
+    time =toc;
+    if feasible
+        [it_kkt] = [it_kkt;it];
+    end
+end
+disp('percentage solved');
+length(it_kkt)/n_sample
+
+save(getUniqueName('it_kkt'),'it_kkt','msd');
